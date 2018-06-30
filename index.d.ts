03758277d4433624857b5bde8c91c80bf2f95507
Mike Joyce <michael.joyce@target.com>
2018-06-30 18:50:25 -0500

    add jsdoc comments to methods

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,16 @@
 declare namespace jest {
   interface Matchers<R> {
+    /**
+     * Expects that passed in mock function has been called at least once with an action
+     * whose type matches expected actionType
+     */
     toBeDispatchedWithActionType: (actionType: string) => R;
+    /**
+     * Expects that passed in mock function has been called with actions whose
+     * type match the order of the expected actionTypes
+     */
+    toBeDispatchedWithActionTypeOrder: (
+      actionTypesInExpectedOrder: string[]
+    ) => R;
   }
 }
